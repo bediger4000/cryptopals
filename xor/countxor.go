0e942c2f8cf400e144eaa8f07b6c3d2942fe8015
Bruce Ediger <bediger@stratigery.com>
2019-08-21 21:27:29 -0600

    Clarity in byte value

--- xor/countxor.go
+++ xor/countxor.go
@@ -5,9 +5,8 @@ func CountAndXor(ciphertext []byte, keyByte byte) ([]byte, int) {
 	cleartext := make([]byte, len(ciphertext))
 	for i, cipherByte := range ciphertext {
 		clearByte := cipherByte ^ keyByte
-		if clearByte >= 0x20 && clearByte <= 0x7e {
-			N++
-		} else if clearByte == '\n' || clearByte == '\t' {
+		if (clearByte >= 0x20 && clearByte <= 0x7e) ||
+			clearByte == '\n' || clearByte == '\t' || clearByte == '\r' {
 			N++
 		}
 		cleartext[i] = clearByte
