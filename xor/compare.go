f38c2355579f2d9766128dce24bad622abd4e95a
Bruce Ediger <bediger8@gmail.com>
2019-08-26 22:31:31 -0600

    Compare two buffers for byte-wise equality

---
+++ xor/compare.go
@@ -0,0 +1,13 @@
+package xor
+
+func CompareBuffers(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, ai := range a {
+		if ai != b[i] {
+			return false
+		}
+	}
+	return true
+}
