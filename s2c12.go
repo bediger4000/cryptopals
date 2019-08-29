923888700e68fb24ff9de38bdef334a431590560
Bruce Ediger <bediger8@gmail.com>
2019-08-28 19:29:18 -0600

    Better 1-cipher-block decrypting

--- s2c12.go
+++ s2c12.go
@@ -3,6 +3,7 @@ package main
 import (
 	"cryptopals/detect"
 	"cryptopals/myaes"
+	"cryptopals/xor"
 	"encoding/base64"
 	"flag"
 	"fmt"
@@ -14,13 +15,12 @@ var GlobalKey []byte
 
 func main() {
 	keyString := flag.String("k", "YELLOW SUBMARINE", "AES key")
-	myString := flag.String("s", "", "prefix string")
 	flag.Parse()
 
-	prefixString := []byte(*myString)
-
 	// encodedString := `Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK`
-	encodedString := `bm93IGlzIHRoZSB0aW1lIGZvciBhbGwgZ29vZCBtZW4K`
+	// encodedString := `bm93IGlzIHRoZSB0aW1lIGZvciBhbGwgZ29vZCBtZW4K`
+	// encodedString := `YWJjZGVmZ2hpamtsbW5vcAo=`
+	encodedString := `YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo=`
 
 	unknownString, err := base64.StdEncoding.DecodeString(encodedString)
 	if err != nil {
@@ -28,7 +28,6 @@ func main() {
 	}
 
 	fmt.Printf("unknownString is %d bytes long\n", len(unknownString))
-	fmt.Printf("prefix        is %d bytes long\n", len(prefixString))
 
 	blockLength := detect.FindCipherBlockLength(myaes.ECBEncode, []byte(*keyString))
 	if blockLength > -1 {
@@ -38,39 +37,67 @@ func main() {
 		return
 	}
 
-	findFirstUnencrypteByte(myaes.ECBEncode, blockLength, []byte(*keyString), unknownString)
+	findUnencryptedBytes(myaes.ECBEncode, blockLength, []byte(*keyString), unknownString)
 }
 
-func findFirstUnencrypteByte(cipher func([]byte, []byte) []byte, blockLength int, key []byte, unknownString []byte) {
-	// prefixLength := blockLength - 1
-	prefixBuffer := make([]byte, blockLength-1)
-	for i := 0; i < blockLength-1; i++ {
-		prefixBuffer[i] = 'A'
-	}
-	fmt.Printf("prefix block:                 %s\n", printBuffer(prefixBuffer))
+func findUnencryptedBytes(cipher func([]byte, []byte) []byte, blockLength int, key []byte, unknownString []byte) {
+	var decipheredBytes []byte
 
-	cipherText := combineAndEncrypt(prefixBuffer, unknownString, key)
-	searchValue := cipherText[blockLength-1]
-	fmt.Printf("Searching for byte with encrypted value 0x%02x\n", cipherText[blockLength-1])
-	fmt.Printf("Block to look for:            %s\n", printBuffer(cipherText[:blockLength]))
+	fmt.Printf("Unknown string: %q\n", unknownString)
+	encrypted := cipher(unknownString, key)
+	blockCount := len(encrypted) / blockLength
 
-	prefixBuffer = make([]byte, blockLength)
-	for i := 0; i < blockLength-1; i++ {
-		prefixBuffer[i] = 'A'
-	}
+	fmt.Printf("%d encrypted blocks\n", blockCount)
 
-	for i := 0; i < 256; i++ {
-		prefixBuffer[blockLength-1] = byte(i)
-		cipherText := cipher(prefixBuffer, key)
-		fmt.Printf("input byte 0x%02x, cipher block %s\n", byte(i), printBuffer(cipherText[:blockLength]))
-		if cipherText[blockLength-1] == searchValue {
-			fmt.Printf("Found searchvalue 0x%x, '%c' plaintext\n", searchValue, prefixBuffer[blockLength-1])
+	//for blockNo := 0; blockNo < blockCount; blockCount++ {
+	for i := blockLength; i > 0; i-- {
+		var prefixBuffer []byte
+		for j := 0; j < i-1; j++ {
+			prefixBuffer = append(prefixBuffer, 'A')
+		}
+		// "AAAAAAA"
+		fmt.Printf("Iter %d, prefix  %s\n", i, printBuffer(prefixBuffer))
+		zork := make([]byte, len(prefixBuffer))
+		copy(zork, prefixBuffer)
+		zork = append(zork, unknownString...)
+		fmt.Printf("Iter %d, combin  %s\n", i, printBuffer(zork))
+
+		encryptedBytes := combineAndEncrypt(cipher, prefixBuffer, unknownString, key)
+		encryptedBytes = encryptedBytes[0:blockLength]
+		fmt.Printf("Encrypted buffr: %s\n", printBuffer(encryptedBytes))
+
+		prefixBuffer = make([]byte, blockLength)
+		for j := 0; j < i-1; j++ {
+			prefixBuffer[j] = 'A'
+		}
+		for j := 0; j < len(decipheredBytes); j++ {
+			prefixBuffer[i-1+j] = decipheredBytes[j]
+		}
+		fmt.Printf("Prefix buffer 2: %s\n", printBuffer(prefixBuffer))
+
+		var candidate int
+		for candidate = 0; candidate < 256; candidate++ {
+			prefixBuffer[blockLength-1] = byte(candidate)
+			fmt.Printf("Prefix buffer 3: %s\n", printBuffer(prefixBuffer))
+			candidateBlock := cipher(prefixBuffer, key)
+			fmt.Printf("candidate %3d:   %s\n", candidate, printBuffer(candidateBlock))
+			if xor.CompareBuffers(candidateBlock[:blockLength], encryptedBytes) {
+				fmt.Printf("Found candidate %3d, '%c'\n", candidate, candidate)
+				decipheredBytes = append(decipheredBytes, prefixBuffer[blockLength-1])
+				break
+			}
+		}
+		if candidate == 256 {
+			fmt.Printf("Did not find a byte that matched\n")
+			break
 		}
 	}
+	fmt.Printf("Decrypted: %q\n", string(decipheredBytes))
+	//}
 }
 
-func combineAndEncrypt(prefixString, unknownString, key []byte) []byte {
-	return myaes.ECBEncode(append(prefixString, unknownString...), key)
+func combineAndEncrypt(cipher func([]byte, []byte) []byte, prefixString, unknownString, key []byte) []byte {
+	return cipher(append(prefixString, unknownString...), key)
 }
 
 func printBuffer(buffer []byte) string {
