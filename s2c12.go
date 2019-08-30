5890c74db73a36965f8b65f2a5abd75ae6c59325
Bruce Ediger <bediger8@gmail.com>
2019-08-29 21:58:18 -0600

    Working, except PKCS#7 padding removal

--- s2c12.go
+++ s2c12.go
@@ -20,7 +20,8 @@ func main() {
 	// encodedString := `Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK`
 	// encodedString := `bm93IGlzIHRoZSB0aW1lIGZvciBhbGwgZ29vZCBtZW4K`
 	// encodedString := `YWJjZGVmZ2hpamtsbW5vcAo=`
-	encodedString := `YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo=`
+	// encodedString := `YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo=`
+	encodedString := `YWJjZGVmZ2hpamtsb21ub3BxcnN0dXZ3eHl6MDEyMzQ1Njc4OUFCQ0RFRkdISUpLTE1OT1BRUlNUVVZXWFla`
 
 	unknownString, err := base64.StdEncoding.DecodeString(encodedString)
 	if err != nil {
@@ -43,57 +44,57 @@ func main() {
 func findUnencryptedBytes(cipher func([]byte, []byte) []byte, blockLength int, key []byte, unknownString []byte) {
 	var decipheredBytes []byte
 
-	fmt.Printf("Unknown string: %q\n", unknownString)
-	encrypted := cipher(unknownString, key)
-	blockCount := len(encrypted) / blockLength
+	blockCount := len(cipher(unknownString, key)) / blockLength
 
 	fmt.Printf("%d encrypted blocks\n", blockCount)
 
-	//for blockNo := 0; blockNo < blockCount; blockCount++ {
-	for i := blockLength; i > 0; i-- {
-		var prefixBuffer []byte
-		for j := 0; j < i-1; j++ {
-			prefixBuffer = append(prefixBuffer, 'A')
-		}
-		// "AAAAAAA"
-		fmt.Printf("Iter %d, prefix  %s\n", i, printBuffer(prefixBuffer))
-		zork := make([]byte, len(prefixBuffer))
-		copy(zork, prefixBuffer)
-		zork = append(zork, unknownString...)
-		fmt.Printf("Iter %d, combin  %s\n", i, printBuffer(zork))
+	for blockNo := 0; blockNo < blockCount; blockNo++ {
 
-		encryptedBytes := combineAndEncrypt(cipher, prefixBuffer, unknownString, key)
-		encryptedBytes = encryptedBytes[0:blockLength]
-		fmt.Printf("Encrypted buffr: %s\n", printBuffer(encryptedBytes))
-
-		prefixBuffer = make([]byte, blockLength)
-		for j := 0; j < i-1; j++ {
-			prefixBuffer[j] = 'A'
-		}
-		for j := 0; j < len(decipheredBytes); j++ {
-			prefixBuffer[i-1+j] = decipheredBytes[j]
-		}
-		fmt.Printf("Prefix buffer 2: %s\n", printBuffer(prefixBuffer))
-
-		var candidate int
-		for candidate = 0; candidate < 256; candidate++ {
-			prefixBuffer[blockLength-1] = byte(candidate)
-			fmt.Printf("Prefix buffer 3: %s\n", printBuffer(prefixBuffer))
-			candidateBlock := cipher(prefixBuffer, key)
-			fmt.Printf("candidate %3d:   %s\n", candidate, printBuffer(candidateBlock))
-			if xor.CompareBuffers(candidateBlock[:blockLength], encryptedBytes) {
-				fmt.Printf("Found candidate %3d, '%c'\n", candidate, candidate)
-				decipheredBytes = append(decipheredBytes, prefixBuffer[blockLength-1])
-				break
+		for i := blockLength; i > 0; i-- {
+			var prefixBuffer []byte
+			for j := 0; j < i-1; j++ {
+				prefixBuffer = append(prefixBuffer, 'A')
 			}
-		}
-		if candidate == 256 {
-			fmt.Printf("Did not find a byte that matched\n")
-			break
+			fmt.Printf("blockNo %d, prefix length %d, encrypting prefix %q\n", blockNo, i, string(prefixBuffer))
+
+			encryptedBytes := combineAndEncrypt(cipher, prefixBuffer, unknownString, key)
+			encryptedBytes = encryptedBytes[blockNo*blockLength : blockNo*blockLength+blockLength]
+			fmt.Printf("Using encryptedBytes[%d:%d]\n", blockNo*blockLength, blockNo*blockLength+blockLength)
+
+			var testBuffer []byte
+			for j := 0; j < i-1; j++ {
+				testBuffer = append(testBuffer, 'A')
+			}
+			for j := 0; j < len(decipheredBytes); j++ {
+				testBuffer = append(testBuffer, decipheredBytes[j])
+			}
+			testBuffer = append(testBuffer, byte(0))
+			fmt.Printf("Block number %d, block length %d\n", blockNo, blockLength)
+			fmt.Printf("Test buffer, length(%d): %q", len(testBuffer), testBuffer)
+			fmt.Printf("cutting out [%d:%d]\n", blockNo*blockLength, blockNo*blockLength+blockLength)
+			testBuffer = testBuffer[blockNo*blockLength : blockNo*blockLength+blockLength]
+			fmt.Printf("Test buffer, length(%d): %q\n", len(testBuffer), testBuffer)
+
+			var candidate int
+			for candidate = 0; candidate < 256; candidate++ {
+
+				testBuffer[blockLength-1] = byte(candidate)
+				candidateBlock := cipher(testBuffer, key)
+
+				if xor.CompareBuffers(candidateBlock[0:blockLength], encryptedBytes) {
+					decipheredBytes = append(decipheredBytes, byte(candidate))
+					break
+				}
+			}
+			if candidate == 256 {
+				fmt.Printf("Block length %d, prefix length %d, block number %d\n", blockLength, i, blockNo)
+				fmt.Printf("Did not find a byte that matched\n")
+				fmt.Printf("%d bytes decrypted so far: %q\n", len(decipheredBytes), string(decipheredBytes))
+				return
+			}
+			fmt.Printf("Decrypted so far: %q\n", string(decipheredBytes))
 		}
 	}
-	fmt.Printf("Decrypted: %q\n", string(decipheredBytes))
-	//}
 }
 
 func combineAndEncrypt(cipher func([]byte, []byte) []byte, prefixString, unknownString, key []byte) []byte {
